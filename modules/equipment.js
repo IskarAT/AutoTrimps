071afed7b71ae4afc91b63d5d4d79f003b2aa2b1
IskarAT <34741447+IskarAT@users.noreply.github.com>
2017-12-25 21:43:51 +0100

    Update equipment.js

--- modules/equipment.js
+++ modules/equipment.js
@@ -400,9 +400,9 @@ function autoLevelEquipment() {
                     buyEquipment(eqName, null, true);
                 }
             }
-            /*var averageWeaponPrice = 0;
+            var averageWeaponPrice = 0;
             //Buy health item, if cost is <5% of average weapon price
-            if (getPageSetting('BuyArmor') && (DaThing.Stat == 'health') && (DaThing.Resource == 'metal'){
+            if (getPageSetting('BuyArmor') && (DaThing.Stat == 'health') && (DaThing.Resource == 'metal')){
                 //Calculate average weapon price
                 averageWeaponPrice = equipCost(gameResource, "Dagger") + equipCost(gameResource, "Mace") + equipCost(gameResource, "Polearm") + equipCost(gameResource, "Battleaxe") + equipCost(gameResource, "Greatsword") + equipCost(gameResource, "Arbalest");
                 averageWeaponPrice = averageWeaponPrice * 3; // Price * 20 / 6 ~ Price * 3
@@ -410,7 +410,7 @@ function autoLevelEquipment() {
                     buyEquipment(eqName, null, true);
                     debug('Leveling cheap equipment ' + eqName + "equips", '*upload3');
                 }
-            }*/
+            }
         }
     }
     postBuy();
