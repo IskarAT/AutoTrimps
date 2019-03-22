c57b22989c1f066ebf5f46a2964ebec30fedd348
IskarAT <34741447+IskarAT@users.noreply.github.com>
2019-03-22 19:02:14 +0100

    Update heirlooms.js
    Max core rarity is 6 in Trimps code

--- modules/heirlooms.js
+++ modules/heirlooms.js
@@ -8,8 +8,8 @@ function autoHeirlooms() {
     if(!heirloomsShown && game.global.heirloomsExtra.length > 0){
       for(var extra in game.global.heirloomsExtra) {
         var theLoom = game.global.heirloomsExtra[extra];
-          if(game.global.heirloomsCarried.length < game.global.maxCarriedHeirlooms && theLoom.type == 'Core' && theLoom.rarity > 6) {
-            // We found a Magmatic / Plagued (or potentially higher) core, carry it; if new tied is added, I will expand it. Unlikely for a few months at least
+          if(game.global.heirloomsCarried.length < game.global.maxCarriedHeirlooms && theLoom.type == 'Core' && theLoom.rarity >= 6) {
+            // We found a rarity 6 core (maximum as of March 2019), carry it; if new tier is added, I will expand it. Unlikely for a few months at least
             selectHeirloom(extra, 'heirloomsExtra');
             carryHeirloom();
           }
