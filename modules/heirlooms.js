7a470f4c319287ab71255a0b8ddf803d29816c46
IskarAT <34741447+IskarAT@users.noreply.github.com>
2019-03-22 08:55:07 +0100

    Update heirlooms.js
    Changed rarity cutoff

--- modules/heirlooms.js
+++ modules/heirlooms.js
@@ -2,14 +2,14 @@
 //Heirloom Functions////////////////////
 ////////////////////////////////////////
 
-// Trimmed hard because it was a bloody mess and I only really need to have Plagued Cores
+// Trimmed hard because it was a bloody mess and I only really need to have high quality Cores
 
 function autoHeirlooms() {
     if(!heirloomsShown && game.global.heirloomsExtra.length > 0){
       for(var extra in game.global.heirloomsExtra) {
         var theLoom = game.global.heirloomsExtra[extra];
-          if(game.global.heirloomsCarried.length < game.global.maxCarriedHeirlooms && theLoom.type == 'Core' && theLoom.rarity > 7) {
-            // We found a Plagued (or potentially higher) core, carry it; if new tied is added, I will expand it. Unlikely for a few months at least
+          if(game.global.heirloomsCarried.length < game.global.maxCarriedHeirlooms && theLoom.type == 'Core' && theLoom.rarity > 6) {
+            // We found a Magmatic / Plagued (or potentially higher) core, carry it; if new tied is added, I will expand it. Unlikely for a few months at least
             selectHeirloom(extra, 'heirloomsExtra');
             carryHeirloom();
           }
