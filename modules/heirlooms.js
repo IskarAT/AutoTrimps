0244d1caf9b5b283c589c4af6fff1b58e2afa53a
IskarAT <34741447+IskarAT@users.noreply.github.com>
2019-08-06 08:53:13 +0200

    Fix

--- modules/heirlooms.js
+++ modules/heirlooms.js
@@ -8,7 +8,7 @@ function autoHeirlooms() {
     if(!heirloomsShown && game.global.heirloomsExtra.length > 0){
       for(var extra in game.global.heirloomsExtra) {
         var theLoom = game.global.heirloomsExtra[extra];
-          if(game.global.heirloomsCarried.length < game.global.maxCarriedHeirlooms && ((theLoom.type == 'Core' && theLoom.rarity >= 6) || (theLoom.rarity >= 9)) {
+          if(game.global.heirloomsCarried.length < game.global.maxCarriedHeirlooms && ((theLoom.type == 'Core' && theLoom.rarity >= 6) || (theLoom.rarity >= 9))) {
             // We found a rarity 6 core (maximum as of March 2019), carry it; if new tier is added, I will expand it. Unlikely for a few months at least
             selectHeirloom(extra, 'heirloomsExtra');
             carryHeirloom();
