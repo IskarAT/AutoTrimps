a773fee06f01c7f9f32dc44f693303ef70b74950
IskarAT <34741447+IskarAT@users.noreply.github.com>
2021-02-07 22:15:33 +0100

    Increment rarity of normal heirlooms to keep
    Already got BiS of the current ones, so...

--- modules/heirlooms.js
+++ modules/heirlooms.js
@@ -8,7 +8,7 @@ function autoHeirlooms() {
     if(!heirloomsShown && game.global.heirloomsExtra.length > 0){
       for(var extra in game.global.heirloomsExtra) {
         var theLoom = game.global.heirloomsExtra[extra];
-          if(game.global.heirloomsCarried.length < game.global.maxCarriedHeirlooms && ((theLoom.type == 'Core' && theLoom.rarity >= 6) || (theLoom.rarity >= 10))) {
+          if(game.global.heirloomsCarried.length < game.global.maxCarriedHeirlooms && ((theLoom.type == 'Core' && theLoom.rarity >= 6) || (theLoom.rarity >= 11))) {
             // We found a rarity 6 core (maximum as of March 2019), carry it; Unlikely for a few months to reach new spire for rarity 7
             selectHeirloom(extra, 'heirloomsExtra');
             carryHeirloom();
